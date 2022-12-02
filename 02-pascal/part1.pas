e64806d7735a74bc717e971bd570f2c9211598e9
Stedoss <29103029+Stedoss@users.noreply.github.com>
2022-12-02 14:11:20 +0000

    Tabs -^> Spaces

--- 02-pascal/part1.pas
+++ 02-pascal/part1.pas
@@ -1,60 +1,60 @@
 uses
-	fgl;
+    fgl;
 
 type
-	TCharIntMap = specialize TFPGMap<char, Integer>;
-	TCharCharMap = specialize TFPGMap<char, char>;
+    TCharIntMap = specialize TFPGMap<char, Integer>;
+    TCharCharMap = specialize TFPGMap<char, char>;
 
 var
-	ret: integer;
+    ret: integer;
 
 function impl(): integer;
 var
-	inputFile: TextFile;
-	shapePointValues: TCharIntMap;
-	winResults: TCharCharMap;
-	currentLine: string;
-	opponentChoice: char;
-	guidedChoice: char;
-	totalPoints: integer;
+    inputFile: TextFile;
+    shapePointValues: TCharIntMap;
+    winResults: TCharCharMap;
+    currentLine: string;
+    opponentChoice: char;
+    guidedChoice: char;
+    totalPoints: integer;
 
 begin
-	shapePointValues := TCharIntMap.Create;
-	shapePointValues['X'] := 1;
-	shapePointValues['Y'] := 2;
-	shapePointValues['Z'] := 3;
+    shapePointValues := TCharIntMap.Create;
+    shapePointValues['X'] := 1;
+    shapePointValues['Y'] := 2;
+    shapePointValues['Z'] := 3;
 
-	winResults := TCharCharMap.Create;
-	winResults['A'] := 'Y';
-	winResults['B'] := 'Z';
-	winResults['C'] := 'X';
+    winResults := TCharCharMap.Create;
+    winResults['A'] := 'Y';
+    winResults['B'] := 'Z';
+    winResults['C'] := 'X';
 
-	totalPoints := 0;
+    totalPoints := 0;
 
-	Assign(inputFile, 'input.txt');
-	reset(inputFile);
+    Assign(inputFile, 'input.txt');
+    reset(inputFile);
 
-	repeat
-		readln(inputFile, currentLine);
+    repeat
+        readln(inputFile, currentLine);
 
-		opponentChoice := currentLine[1];
-		guidedChoice := currentLine[3];
+        opponentChoice := currentLine[1];
+        guidedChoice := currentLine[3];
 
-		totalPoints += shapePointValues[guidedChoice];
+        totalPoints += shapePointValues[guidedChoice];
 
-		if opponentChoice = chr(ord(guidedChoice)-23) then
-			totalPoints += 3
-		else if winResults[opponentChoice] = guidedChoice then
-			totalPoints += 6
-	until (eof(inputFile) or (currentLine = ''));
+        if opponentChoice = chr(ord(guidedChoice)-23) then
+            totalPoints += 3
+        else if winResults[opponentChoice] = guidedChoice then
+            totalPoints += 6
+    until (eof(inputFile) or (currentLine = ''));
 
-	Close(inputFile);
+    Close(inputFile);
 
-	impl := totalPoints;
+    impl := totalPoints;
 end;
 
 begin
-	ret := impl;
-	WriteLn(ret);
-	WriteLn(ret = 9759);
+    ret := impl;
+    WriteLn(ret);
+    WriteLn(ret = 9759);
 end.
--- 02-pascal/part2.pas
+++ 02-pascal/part2.pas
@@ -1,75 +1,75 @@
 uses
-	fgl;
+    fgl;
 
 type
-	TCharIntMap = specialize TFPGMap<char, Integer>;
-	TCharCharMap = specialize TFPGMap<char, char>;
+    TCharIntMap = specialize TFPGMap<char, Integer>;
+    TCharCharMap = specialize TFPGMap<char, char>;
 
 var
-	ret: integer;
+    ret: integer;
 
 function impl(): integer;
 var
-	inputFile: TextFile;
-	shapePointValues: TCharIntMap;
-	winResults: TCharCharMap;
-	currentLine: string;
-	opponentChoice: char;
-	outcome: char;
-	totalPoints: integer;
-	i: integer;
+    inputFile: TextFile;
+    shapePointValues: TCharIntMap;
+    winResults: TCharCharMap;
+    currentLine: string;
+    opponentChoice: char;
+    outcome: char;
+    totalPoints: integer;
+    i: integer;
 
 begin
-	shapePointValues := TCharIntMap.Create;
-	shapePointValues['X'] := 1;
-	shapePointValues['Y'] := 2;
-	shapePointValues['Z'] := 3;
+    shapePointValues := TCharIntMap.Create;
+    shapePointValues['X'] := 1;
+    shapePointValues['Y'] := 2;
+    shapePointValues['Z'] := 3;
 
-	winResults := TCharCharMap.Create;
-	winResults['A'] := 'Y';
-	winResults['B'] := 'Z';
-	winResults['C'] := 'X';
+    winResults := TCharCharMap.Create;
+    winResults['A'] := 'Y';
+    winResults['B'] := 'Z';
+    winResults['C'] := 'X';
 
-	totalPoints := 0;
+    totalPoints := 0;
 
-	Assign(inputFile, 'input.txt');
-	reset(inputFile);
+    Assign(inputFile, 'input.txt');
+    reset(inputFile);
 
-	repeat
-		readln(inputFile, currentLine);
+    repeat
+        readln(inputFile, currentLine);
 
-		opponentChoice := currentLine[1];
-		outcome := currentLine[3];
+        opponentChoice := currentLine[1];
+        outcome := currentLine[3];
 
-		if outcome = 'Y' then //draw
-		begin
-			totalPoints += 3;
-			totalPoints += shapePointValues[chr(ord(opponentChoice)+23)];
-		end
-		else if outcome = 'Z' then //win
-		begin
-			totalPoints += 6;
-			totalPoints += shapePointValues[winResults[opponentChoice]];
-		end
-		else
-		begin
-			for i := 0 to winResults.Count - 1 do
-			begin
-				if winResults.Data[i] = chr(ord(opponentChoice)+23) then
-				begin
-					totalPoints += shapePointValues[chr(ord(winResults.Keys[i])+23)];
-				end;
-			end;
-		end;
-	until (eof(inputFile));
+        if outcome = 'Y' then //draw
+        begin
+            totalPoints += 3;
+            totalPoints += shapePointValues[chr(ord(opponentChoice)+23)];
+        end
+        else if outcome = 'Z' then //win
+        begin
+            totalPoints += 6;
+            totalPoints += shapePointValues[winResults[opponentChoice]];
+        end
+        else
+        begin
+            for i := 0 to winResults.Count - 1 do
+            begin
+                if winResults.Data[i] = chr(ord(opponentChoice)+23) then
+                begin
+                    totalPoints += shapePointValues[chr(ord(winResults.Keys[i])+23)];
+                end;
+            end;
+        end;
+    until (eof(inputFile));
 
-	Close(inputFile);
+    Close(inputFile);
 
-	impl := totalPoints;
+    impl := totalPoints;
 end;
 
 begin
-	ret := impl;
-	WriteLn(ret);
-	WriteLn(ret = 12429);
+    ret := impl;
+    WriteLn(ret);
+    WriteLn(ret = 12429);
 end.
