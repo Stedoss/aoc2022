409d48d389e26d300b3080fa4376b82cb777f8e7
Stedoss <29103029+Stedoss@users.noreply.github.com>
2022-12-02 00:35:23 +0000

    Add 01-dart implementation

---
+++ 01-dart/part1.dart
@@ -0,0 +1,36 @@
+import "dart:io";
+
+void main(List<String> args) async {
+    final result = impl();
+    print(result);
+    print(result == 72240);
+}
+
+int impl() {
+    final fileLines = File("input.txt").readAsLinesSync();
+
+    // If the file doesn't have a new line at the end, make one
+    if (fileLines.last.isEmpty) {
+        fileLines.add("");
+    }
+
+    int fattestElf = 0, fattestElfCal = 0;
+    int currentElf = 1;
+    int currentElfCal = 0;
+
+    for (var line in fileLines) {
+        if (line.isEmpty) {
+            if (currentElfCal > fattestElfCal) {
+                fattestElfCal = currentElfCal;
+                fattestElf = currentElf;
+            }
+
+            currentElfCal = 0;
+            currentElf++;
+        } else {
+            currentElfCal += int.parse(line);
+        }
+    }
+
+    return fattestElfCal;
+}
---
+++ 01-dart/part2.dart
@@ -0,0 +1,39 @@
+import "dart:io";
+
+void main() async {
+    final result = impl();
+    print(result);
+    print(result == 210957);
+}
+
+int impl() {
+    final fileLines = File("input.txt").readAsLinesSync();
+
+    // If the file doesn't have a new line at the end, make one
+    if (fileLines.last.isEmpty) {
+        fileLines.add("");
+    }
+
+    final elfCalories = <int>[];
+
+    int currentElfCal = 0;
+
+    for (var line in fileLines) {
+        if (line.isEmpty) {
+            elfCalories.add(currentElfCal);
+
+            currentElfCal = 0;
+            continue;
+        } else {
+            currentElfCal += int.parse(line);
+        }
+    }
+
+    elfCalories.sort(((a, b) => b - a));
+
+    elfCalories.forEach((element) {print(element);});
+
+    final topThreeCalories = elfCalories.take(3).reduce((a, b) => a + b);
+
+    return topThreeCalories;
+}
